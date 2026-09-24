cbfb22c87d3bdcff1bd8b9d1454fe29a742932e4
Yiğit Aslan <yigit__aslan@outlook.com>
2026-09-24 16:10:20 +0300

    feat: initial project structure with JDK 21, Spring Boot and enterprise dependencies

---
+++ src/test/java/com/yigitaslan/call_center_system/CallCenterSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yigitaslan.call_center_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallCenterSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
